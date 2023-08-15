7085a5ddc626e64ca35716a59bfd0dd775a3bfa8
SanAlfredo <alfredo.2009.8@gmail.com>
2023-08-15 17:40:12 -0400

    V 1.1 factorial, derivada, raiz cubica agregados

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
   derivative,
   e,
   evaluate,
+  factorial,
   log,
   log10,
   round,
@@ -21,7 +22,7 @@ function App() {
   };
   const calcularRes = () => {
     if (input) {
-      setInput(evaluate(input));
+      setInput("" + evaluate(input));
     } else {
       alert("No hay valores para calcular!!!");
     }
@@ -78,14 +79,51 @@ function App() {
   };
   const derivar = () => {
     if (input) {
-      setInput(derivative(input,'x'));
+      setInput("" + derivative(input, "x"));
+      //console.log(input);
     } else {
       alert("No hay valores para derivar!!!");
     }
   };
+  const quitarValor = () => {
+    if (input) {
+      setInput(input.substring(0, input.length - 1));
+      //console.log(input);
+    } else {
+      alert("No hay valores para quitar!!!");
+    }
+  };
+  const calculoFactorial = () => {
+    if (input) {
+      setInput(input+"!");
+    } else {
+      alert("No hay valores para el factorial!!!");
+    }
+  };
   return (
     <>
-      <h1>Calculadora</h1>
+      <div className="Instrucciones">
+        <h2>Calculadora Científica</h2>
+        <p>Diseñada por: Ing. Alfredo Valverde Aranibar</p>
+        <h3>Instruciones de uso</h3>
+        <ul>
+          <h4>funciones que requieren del igual "="</h4>
+          <li>Las funciones básicas de "+,-,*,/,10^,e^,^(1/3),n!" requieren del igual</li>
+          <li>sin,cos,tan se usan de la siguiente forma:
+            ejemplo sin(45deg) y luego igual
+          </li>
+          <li>^ sirve para elevar un numero por ejemplo: 2^2 = 4</li>
+          <h4>funciones que no requiren del igual</h4>
+          <li>Rnd: redondea el numero a 2 decimales</li>
+          <li>Log,Ln requiren de un valor para calcular</li>
+          <li>Sqrt es la raiz y requiere de un valor para calcular</li>
+          <li>+/- cambia el signo del numero</li>
+          <li>d / dx deriva la funcion respecto de x requiere de una funcion en x, por ejemplo: x^2+x</li>
+          <li>i agrega la letra i de numero complejo a los valores</li>
+          <li>del borra el ultimo valor introducido hasta vaciar la pantalla</li>
+          <li>Limpiar Pantalla deja limpia la pantalla para volver a empezar</li>
+        </ul>
+      </div>
       <div className="App">
         <div className="contenedor-calcu">
           <Pantalla input={input} />
@@ -114,10 +152,16 @@ function App() {
               <BotonEspecial manejarClick={cambio}>+ / -</BotonEspecial>
             </div>
             <div className="fila-especial">
-              <BotonEspecial manejarClick={derivar}>dx</BotonEspecial>
+              <BotonEspecial manejarClick={agregarValor}>10^</BotonEspecial>
+              <BotonEspecial manejarClick={agregarValor}>e^</BotonEspecial>
+              <BotonEspecial manejarClick={agregarValor}>^(1/3)</BotonEspecial>
+              <BotonEspecial manejarClick={calculoFactorial}>n!</BotonEspecial>
+            </div>
+            <div className="fila-especial">
+              <BotonEspecial manejarClick={derivar}>d / dx</BotonEspecial>
               <BotonEspecial manejarClick={agregarValor}>x</BotonEspecial>
               <BotonEspecial manejarClick={agregarValor}>i</BotonEspecial>
-              <BotonEspecial manejarClick={agregarValor}>,</BotonEspecial>
+              <BotonEspecial manejarClick={quitarValor}>del</BotonEspecial>
             </div>
           </div>
           <div className="fila">
